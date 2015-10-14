7b2692df8913b45e666f5fd326b0b2c8d5573773
Edouard DUPIN <yui.heero@gmail.com>
2015-10-14 21:21:03 +0200

    [DEV] update new lutin 0.8.0

--- lutin_edtaa3.py
+++ lutin_edtaa3.py
@@ -2,20 +2,31 @@
 import lutin.module as module
 import lutin.tools as tools
 
+
+def get_type():
+	return "LIBRARY"
+
 def get_desc():
 	return "edtaa3 library (create distance field from image)"
 
-def create(target):
-	# module name is 'edn' and type binary.
-	my_module = module.Module(__file__, 'edtaa3', 'LIBRARY')
-	# add the file to compile:
+def get_licence():
+	return "BSD-2"
+
+def get_maintainer():
+	return ["Stefan Gustavson <stefan.gustavson@gmail.com>"]
+
+def get_version():
+	return [0,5]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
 	my_module.add_src_file([
 		'edtaa3/edtaa3func.c'
 		])
 	my_module.add_header_file([
 		'edtaa3/edtaa3func.h'
 		])
-	my_module.compile_version_CC(1999)
+	my_module.compile_version("C", 1999)
 	my_module.add_path(tools.get_current_path(__file__))
 	return my_module
 
